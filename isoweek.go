31377db0d390b23733517084ed3675bd08624ac2
Janne Snabb <snabb@epipe.com>
2016-08-02 23:30:28 +0300

    Simplify calculation.

--- isoweek.go
+++ isoweek.go
@@ -8,7 +8,7 @@ package isoweek
 import "time"
 
 // isoWeekday returns the ISO weekday number of given day.
-// (1 = mon, 2 = tue,.. 7 = sun)
+// (1 = Mon, 2 = Tue,.. 7 = Sun)
 func isoWeekday(year, month, day int) (weekday int) {
 	t := time.Date(year, time.Month(month), day, 0, 0, 0, 0, time.UTC)
 	weekday = int(t.Weekday())
@@ -18,16 +18,17 @@ func isoWeekday(year, month, day int) (weekday int) {
 	return weekday
 }
 
-// startOrdinal returns the ordinal day number of Monday of the
-// given week. Return value may be negative or bigger than number of
+// startOrdinal returns the ordinal day number (counting from 0) of Monday
+// of the given week. Return value may be negative or bigger than number of
 // days in a year.
 func startOrdinal(year, week int) (ordinal int) {
-	return week*7 - isoWeekday(year, 1, 4) - 2
+	return week*7 - isoWeekday(year, 1, 4) - 3
 }
 
 // StartTime returns the starting time (Monday 00:00) of the given ISO week.
 func StartTime(wyear, week int, loc *time.Location) (start time.Time) {
-	return time.Date(wyear, 1, 1, 0, 0, 0, 0, loc).AddDate(0, 0, startOrdinal(wyear, week)-1)
+	return time.Date(wyear, 1, 1, 0, 0, 0, 0, loc).AddDate(0, 0,
+		startOrdinal(wyear, week))
 }
 
 // StartDate returns the starting date (Monday) of the given ISO week.
